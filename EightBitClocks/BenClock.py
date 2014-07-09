9b9e9166b172d7e5f946cf6ce35934bc5d79da30
Ben Schwarz <schwarzfunk@gmail.com>
2014-07-09 11:26:24 +0200

    Add Bens Clock

---
+++ EightBitClocks/BenClock.py
@@ -0,0 +1,75 @@
+"""
+Displays a clock in local time
+
+Time may be modified by optional offset parameter to Clock
+constructor (in hours), e.g. BenClock(-1) 
+"""
+
+class Hand:
+    length = 20
+    maxLength = 50
+    strokeWeight = 20
+    rotation = 0
+    red = 0
+    green = 0
+    blue = 0
+    
+    def draw(self):
+        self.update()
+        pushMatrix()
+        #rotate(self.rotation - HALF_PI) # Processing starts at 0 deg = horizontal
+        strokeWeight(self.strokeWeight)
+        stroke(self.red, self.green, self.blue)
+        line(0, 0, self.length, 0)
+        popMatrix()
+    
+    def update(self):
+        pass
+
+class SecondsHand(Hand):
+    length = 120
+    red = 255
+    green = 255
+    
+    def update(self):
+        translate(0, self.strokeWeight);
+        self.length = map(second(), 0, 60, 0, self.maxLength)
+
+class MinutesHand(Hand):
+    length = 100
+    red = 255
+    
+    def update(self):
+        self.length = map(minute(), 0, 60, 0, self.maxLength)
+
+class HoursHand(Hand):
+    length = 50
+    offset = 0
+    
+    def __init__(self, offset = 0):
+        self.offset = offset
+        
+    def update(self):
+        translate(0, -1 * self.strokeWeight);
+        self.length = map(hour() + self.offset, 0, 12, 0, self.maxLength)
+
+class BenClock:
+    offset = 0
+    
+    secondsHand = SecondsHand()
+    minutesHand = MinutesHand()
+    #hoursHand = HoursHand()
+    
+    def __init__(self, offset = 0):
+        self.offset = offset
+        self.hoursHand = HoursHand(self.offset)
+
+    def draw(self): # TODO Translate() here, make x/y optional arguments
+        self.minutesHand.draw()
+        # TODO skip rotate + push/popMatrix if offset == 0
+        pushMatrix()
+        # rotate(self.offset * TWO_PI / 12)
+        self.hoursHand.draw()
+        popMatrix()
+        self.secondsHand.draw()
+
--- EightBitClocks/EightBitClocks.pyde
+++ EightBitClocks/EightBitClocks.pyde
@@ -5,8 +5,10 @@
 
 # Hier sollen alle Uhren eingefügt werden
 from JensClock import JensClock
+from BenClock import BenClock
 
 clockJens = JensClock()
+clockBen = BenClock()
 
 
 def setup():
@@ -19,4 +21,6 @@ def draw():
     # Hier als Beispiel die Uhr zum Zeichnen einfügen
     translate(width * 1/4.0, height/2)
     clockJens.draw()
+    translate(width * 2/4.0, 0)
+    clockBen.draw()
 
